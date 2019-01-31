06a37e34edf13eeb4750708730accbe6e0e4f659
Sergey Green <green.sergey@gmail.com>
2019-01-31 15:34:53 +0100

    intermediate commit

--- rest_helper.py
+++ rest_helper.py
@@ -2,6 +2,7 @@
 """
 Usage:
     rest_helper.py -n <num> -c <config>
+
 Options:
     -n, --num <num>         Number between 1 and 4
     -c, --config <config>   Config file
@@ -9,12 +10,26 @@ Options:
 """
 
 import os.path
+import string
 from docopt import docopt
 
 try:
     import configparser
-except:
+except ImportError:
     from six.moves import configparser
+    
+try:
+    from urllib.parse import urlparse
+except ImportError:
+     from urlparse import urlparse
+   
+class OutsideRange(Exception):
+    """The value is outside the range..."""
+    pass
+        
+class NonExistent(Exception):
+    """File not found..."""
+    pass
 
 class RestHelper(object):    
     def __init__(self):
@@ -24,34 +39,39 @@ class RestHelper(object):
         arguments = docopt(__doc__)
         self.num = int(arguments['--num'])
         self.config = arguments['--config']
-                    
-    def ifNum(self):
-        if not 1 <= self.num <= 4:
-            print ("%s is outside [1-4] range .." % self.num)
+        
+    def verifyArgs(self):
+        try:
+            if not 1 <= self.num:
+                raise OutsideRange
+            if not os.path.isfile(self.config):
+                raise NonExistent
+        except OutsideRange:
+            print ("%s is less than 1 .." % self.num)
             exit(1)
-            
-    def ifConfig(self):
-        if not os.path.isfile(self.config):
+        except NonExistent:
             print ("%s was not found .." % self.config)
             exit(1)
-            
+                
     def readConfig(self):
         self.parser.read(self.config)
         count = 0
-        
+            
         username = self.parser.get('Data', 'username')
         urlpath  = self.parser.get('Data', 'urlpath')
         for name, value in self.parser.items("Urls"):
             count += 1
-            print ("%s@%s%s" % (username,value,urlpath))
+            uo = urlparse(value)
+                
+            print "%s://%s@%s%s" % (uo.scheme, username, uo.netloc, urlpath )
+                
             if count == int(self.num):
                 break
                     
 def main():
     rh = RestHelper()
     rh.getArgs()
-    rh.ifNum()
-    rh.ifConfig()
+    rh.verifyArgs()
     rh.readConfig()
 
 if __name__ == '__main__':
